2257f20ff98e3e72b05f5fc65007bec5ea81140e
Martin Kutzner <mfkutz@gmail.com>
2023-05-19 20:43:25 -0300

    some arrays

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CustomForm from "./components/CustomForm"
 function App() {
   
   return (
-   <div className="bg-varRed bg-mobile lg:bg-desktop p-6 min-h-screen" >
+   <div className="bg-varRed bg-mobile lg:bg-desktop p-6 min-h-screen lg:flex lg:justify-center" >
         <CustomForm />
    </div>
   )
--- src/components/CustomForm.jsx
+++ src/components/CustomForm.jsx
@@ -1,17 +1,17 @@
 
 const CustomForm = () => {
     return (
-        <div className=" flex justify-center items-center flex-col lg:flex-row">
+        <div className="flex justify-center items-center flex-col lg:flex-row min-w-full lg:justify-center">
 
-            <div className="text-varWhite text-center ">
-                <h1 className="poppins-b text-[28px] p-2 pt-16 leading-9">Learn to code by watching others</h1>
-                <p className="p-1 pt-4 poppins-m leading-[27px]">See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
+            <div className="text-varWhite text-center flex flex-col justify-center max-w-md lg:max-w-[600px] lg:p-4">
+                <h1 className="poppins-b text-[28px] p-2 pt-16 leading-9 lg:text-[50px] lg:text-left lg:leading-[55px] lg:pt-5">Learn to code by watching others</h1>
+                <p className="p-1 pt-4 poppins-m leading-[27px] lg:max-w-md lg:min-w-full lg:text-left">See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
                     but understanding how developers think is invaluable. </p>
             </div>
 
-            <div>
-                <button className="bg-varBlue mt-14 py-5 px-16 rounded-lg text-varWhite shadowM"><span className="poppins-sb">Try it free 7 days</span> then $20/mo. thereafter</button>
-                <form className="flex flex-col bg-white mt-6 rounded-lg p-6 gap-4 shadowM">
+            <div className="lg:min-w-[530px]">
+                <button className="bg-varBlue mt-14 py-5 px-16 rounded-lg text-varWhite shadowM lg:min-w-[530px]"><span className="poppins-sb">Try it free 7 days</span> then $20/mo. thereafter</button>
+                <form className="flex flex-col bg-white mt-6 rounded-lg p-6 gap-4 shadowM lg:p-10 lg:gap-5">
                     <input
                         type="text"
                         placeholder="First Name"
